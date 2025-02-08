43a6dfe768bd8e26e3ad027f4ab55235cde6dc0a
Kiranmayee Lokam <kiranmayee.lokam.419@my.csun.edu>
2025-02-07 18:54:52 -0800

    Your descriptive commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { WeatherForecast } from "./weather-forecast";
+import { environment } from "../environments/environment.development";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
+  imports: [
+    NavBarComponent,
+    RouterOutlet
+  ]
 })
-export class AppComponent {
-  title = 'KiranmayeeClient';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+      }
+
+  title = 'angularapp1.client';
 }
